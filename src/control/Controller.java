af635ffc8691beb212f01858140a804c24d02034
KReguieg <khaled.reguieg@gmail.com>
2015-04-09 11:01:13 +0200

    [ABOUTWINDOW] Added a new window/stage named about with icon and information about the application

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -1,12 +1,20 @@
 package control;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
 
     @FXML
     public void onClickExit(){
         System.out.println("Exit application");
+        System.exit(0);
     }
 
     @FXML
@@ -22,5 +30,22 @@ public class Controller {
     @FXML
     public void onClickAbout(){
         System.out.println("Paper Jets v0.1");
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("../view/about.fxml"));
+            Stage aboutWindow = new Stage();
+            aboutWindow.setTitle("About PaperJets");
+            aboutWindow.setScene(new Scene(root, 300, 250));
+            aboutWindow.getIcons().add(new Image("res/ic_send_black_48dp.png"));
+            aboutWindow.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    public void onExitAbout(){
+        System.exit(0);
     }
 }
