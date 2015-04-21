4f629762bc8fbc1bd9d193bf64f9d5b36481d91a
KReguieg <khaled.reguieg@gmail.com>
2015-04-21 15:06:49 +0200

    [Messages] successfuly added example messages and added them to the listView

--- src/control/ContentController.java
+++ src/control/ContentController.java
@@ -1,20 +1,22 @@
 package control;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import model.Message;
 import model.MessageImportance;
+import model.MessageStakeholder;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 /**
  * Created by Khaled_000 on 14.04.2015.
  */
 public class ContentController {
 
-    private Main main;
+    private ObservableList<Message> messageData = FXCollections.observableArrayList();
 
     public ContentController() {
 
@@ -38,11 +40,15 @@ public class ContentController {
     @FXML
     private TableColumn<Message, String> subjectColoumnId;
 
-    public void initialize(){
+    public void initialize() {
+        System.out.println("Initialize Controller...");
+        createExampleMessages();
         loadTableItems();
+        tableViewId.setItems(getMessageData());
+        System.out.println("Controller initialized...");
     }
 
-    public void loadTableItems(){
+    public void loadTableItems() {
         priorityColoumnId.setCellValueFactory(cellData -> cellData.getValue().importanceOfMessageProperty());
         receivedColoumnId.setCellValueFactory(cellData -> cellData.getValue().receivedAtProperty());
         readColoumnId.setCellValueFactory(cellData -> cellData.getValue().readStatusProperty());
@@ -50,8 +56,57 @@ public class ContentController {
         subjectColoumnId.setCellValueFactory(cellData -> cellData.getValue().subjectProperty());
     }
 
-    public void setMain(Main main) {
-        this.main = main;
-        tableViewId.setItems(main.getMessageData());
+    /**
+     * This method creates the 1st example Message, by setting the values
+     * of the message instance over the setter methods
+     */
+    public void createExampleMessage1() {
+        Message m1 = new Message();
+        m1.setId("1");
+        m1.setImportanceOfMessage(MessageImportance.LOW);
+        m1.setReadStatus(true);
+        m1.setReceivedAt(LocalDateTime.now());
+        m1.setSubject("Omg Zeig das an!");
+        m1.setText("BLABLABLABLA");
+        MessageStakeholder ms1 = new MessageStakeholder();
+        ms1.setName("Khaled Reguieg");
+        ms1.setMailAddress("Khaled.Reguieg@gmail.com");
+        m1.setSender(ms1);
+        messageData.add(m1);
+    }
+
+    /**
+     * This method creates the 2nd example Message, by setting the values
+     * of the message instance over the setter methods
+     */
+    public void createExampleMessage2() {
+        Message m2 = new Message();
+        m2.setId("2");
+        m2.setImportanceOfMessage(MessageImportance.HIGH);
+        m2.setReadStatus(true);
+        m2.setReceivedAt(LocalDateTime.now());
+        m2.setSubject("Omg Zeig das an!");
+        m2.setText("YAAAAAAAAAAAAAAAAAAAAAAAAY");
+        MessageStakeholder ms1 = new MessageStakeholder();
+        ms1.setName("Khaled Reguieg");
+        ms1.setMailAddress("Khaled.Reguieg@gmail.com");
+        m2.setSender(ms1);
+        messageData.add(m2);
+    }
+
+    /**
+     * This method calls ALL createExampleMessage methods
+     */
+    public void createExampleMessages() {
+        createExampleMessage1();
+        createExampleMessage2();
+    }
+
+    /**
+     * Returns the observableList object
+     * @return ObservableList<Message>
+     */
+    public ObservableList<Message> getMessageData() {
+        return messageData;
     }
 }
--- src/control/Main.java
+++ src/control/Main.java
@@ -1,31 +1,20 @@
 package control;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import model.Message;
-import model.MessageImportance;
-
-import java.io.IOException;
-import java.util.Observable;
 
 public class Main extends Application {
 
-    private ObservableList<Message> messageData = FXCollections.observableArrayList();
     private BorderPane root;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         root = FXMLLoader.load(getClass().getResource("../view/root.fxml"));
         primaryStage.setTitle("PaperJets");
         primaryStage.getIcons().add(new Image("res/ic_send_black_48dp.png"));
@@ -37,29 +26,20 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    public Main() {
-        messageData.add(new Message());
-        messageData.add(new Message());
-    }
-
-    public ObservableList<Message> getMessageData() {
-        return messageData;
-    }
-
-    public void showMessageOverview(){
-        try {
-            FXMLLoader loader  = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("../view/content.fxml"));
-            AnchorPane messageOverview = (AnchorPane) loader.load();
-
-            root.setCenter(messageOverview);
-
-            ContentController controller = loader.getController();
-            controller.setMain(this);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    public void showMessageOverview() {
+//        try {
+//            FXMLLoader loader = new FXMLLoader();
+//            loader.setLocation(Main.class.getResource("../view/content.fxml"));
+//            AnchorPane messageOverview = (AnchorPane) loader.load();
+//
+//            root.setCenter(messageOverview);
+//
+//            ContentController controller = loader.getController();
+//            controller.setMain(this);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     public static void main(String[] args) {
         launch(args);
